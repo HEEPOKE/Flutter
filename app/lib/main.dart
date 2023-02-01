0da97df95dd8a412a8f5ad0643ab9966ad9d6b2b
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-02-01 16:30:52 +0700

    :money_with_wings: Refactor appbar

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-import './widgets/AppBar.dart';
+import 'widgets/app_bar.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: MyAppBar(),
+        appBar: const MyAppBar(),
         body: Container(),
       ),
     );
--- app/lib/widgets/app_bar.dart
+++ app/lib/widgets/app_bar.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
 
-const MyAppBar();
-
 class MyAppBar extends StatelessWidget {
+  const MyAppBar({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           leading: IconButton(
-            icon: Icon(Icons.menu),
+            icon: const Icon(Icons.menu),
             onPressed: () {},
           ),
-          title: Text('My App'),
+          title: const Text('My App'),
           actions: <Widget>[
             Container(
               width: 40,
               height: 40,
-              decoration: BoxDecoration(
+              decoration: const BoxDecoration(
                 shape: BoxShape.circle,
                 image: DecorationImage(
-                  image: NetworkImage(
-                      'https://picsum.photos/200'),
+                  image: NetworkImage('https://picsum.photos/200'),
                   fit: BoxFit.fill,
                 ),
               ),
