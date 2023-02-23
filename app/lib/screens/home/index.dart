5c20c6fb4dde143aa7f32936b54c0c31b157e2af
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-02-23 10:22:46 +0700

    Update index.dart

--- app/lib/screens/home/index.dart
+++ app/lib/screens/home/index.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
+import '../../widgets/app_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({required Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
-      body: const Center(
+    return const Scaffold(
+      appBar: MyAppBar(),
+      body: Center(
         child: Text(
           'Hello, world!',
           textDirection: TextDirection.ltr,
