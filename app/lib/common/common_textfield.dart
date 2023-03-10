9da908ad4b1fcaeab3eb976abe37b3f1b1bc34bd
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-03-10 12:15:09 +0700

    Create common_textfield.dart

---
+++ app/lib/common/common_textfield.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final IconData prefixIcon;
+  final bool obscureText;
+  final String errorText;
+
+  const CustomTextField({
+    Key? key,
+    required this.controller,
+    required this.hintText,
+    required this.prefixIcon,
+    this.obscureText = false,
+    this.errorText = '',
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        hintText: hintText,
+        prefixIcon: Icon(prefixIcon, color: Colors.deepPurpleAccent),
+        focusedBorder: OutlineInputBorder(
+          borderSide:
+              const BorderSide(color: Colors.deepPurpleAccent, width: 2.0),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.grey, width: 1.0),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        errorText: errorText.isNotEmpty ? errorText : null,
+        errorStyle: const TextStyle(color: Colors.red),
+      ),
+    );
+  }
+}
